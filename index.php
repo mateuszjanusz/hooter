80d0a56c3ca43dc4e5b6e71d0a5c17ea0aff19ee
Mateusz Janusz <mjanu001@gold.ac.uk>
2017-03-02 16:09:28 +0000

    set up index page and added bootstrap

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	error_reporting( E_ALL );
+	ini_set( "display_errors", 1 );
+
+	include_once "./config.php"; 
+	$db = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName) or die(mysqli_error());
+
+
+	$title = "Hooter";
+	$css="css/bootstrap.min.css";
+
+	echo "Hello";
+
+	// include_once "templates/header.php";
+	// include_once "templates/admin/admin-navigation.php";
+	// $navigation = isset( $_GET['page'] );
+	// if ( $navigation ) {
+	//         $contrl = $_GET['page']; //load corresponding controller
+	// } else {
+	// 	$contrl = "entries"; //or load default controller
+	// }
+	// include_once "controllers/admin/$contrl.php"; //load the controller
+	// include_once "templates/footer.php";
+?>
