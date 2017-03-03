35d74eb47c24d0be2c8679402fc395d10656e7f1
Mateusz Janusz <mjanu001@gold.ac.uk>
2017-03-03 00:23:56 +0000

    little change in posts layout

--- templates/nav.php
+++ templates/nav.php
@@ -1,9 +1,9 @@
 <?php
 $out = "
+<ul class='nav nav-tabs'>
 <a class='navbar-brand' href='#'>
             <img alt='Hooter' src='./logo.png'>
 </a>
-<ul class='nav nav-tabs'>
   <li role='presentation' class='active'><a href='#'>Home</a></li>
   <li role='presentation'><a href='#'>Profile</a></li>
   <li role='presentation'><a href='#'>Trends</a></li>
--- views/posts-html.php
+++ views/posts-html.php
@@ -8,12 +8,11 @@ while ( $post = $posts->fetchObject() ) {
 	//create a list element <li> for each of the entries
 	$postsHTML .= 
 	"<li class='list-group-item'><div class='panel panel-default'>
-        <div class='panel-heading'>$post->username is saying:</div>
+        <div class='panel-heading'>
+        <h4>$post->username <small> $post->date_created</small></h4>
+        </div>
         <div class='panel-body'>
             <h5>$post->post_text</h5>
-        </div>
-        <div class='panel-footer'>
-            <h6>Created: $post->date_created</h6>
         </div>
      </div>
      
