282bf38b030c16ee6e3c03e7d83c25f401249ff5
Mateusz Janusz <mjanu001@gold.ac.uk>
2017-03-25 18:13:10 +0000

    hide delete button for guests

--- views/posts-html.php
+++ views/posts-html.php
@@ -6,24 +6,26 @@ if ( $postsFound === false ) {
 $postsHTML = "<ul id='posts'>";
 while ( $post = $posts->fetchObject() ) {
 	//create a list element <li> for each of the entries
-	$postsHTML .= 
+	$postsHTML .=
 	"<li class='list-group-item'><div class='panel panel-default'>
         <div class='panel-heading'>
-            <h4>$post->username <small> $post->date_created</small></h4>
-            <form method='post' action='index.php?page=home' id='post_editor'>
-                <input type='hidden' name='user_id' value='$post->user_id' />
-                <input type='hidden' name='post_id' value='$post->post_id' />
-                <button type='submit' class='pull-right' name='delete'>
-                    <span class='glyphicon glyphicon-trash'></span>
-                </button>
-            </form>
-        </div>
+            <h4>$post->username <small> $post->date_created</small></h4>";
+            //show delete button only if user is logged in
+                if ($user->isLoggedIn()) {
+                  $postsHTML .= "<form method='post' action='index.php?page=home' id='post_editor'>
+                        <input type='hidden' name='user_id' value='$post->user_id' />
+                        <input type='hidden' name='post_id' value='$post->post_id' />
+                        <button type='submit' class='pull-right' name='delete'>
+                            <span class='glyphicon glyphicon-trash'></span>
+                        </button>
+                  </form>";
+              	}
+  $postsHTML .="</div>
         <div class='panel-body'>
             <h5>$post->post_text</h5>
         </div>
      </div>
-     
-     </li>"; 
+     </li>";
 }
 $postsHTML .= "</ul>";
 
