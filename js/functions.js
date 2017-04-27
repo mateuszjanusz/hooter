820cd1c917d53239a7375b5a591ae2410a6447a1
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-04-27 20:47:34 +0100

    simple js for testing

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+
+function emptyPost() {
+    alert("Your post cannot be empty!");
+}
+
+function emptySearch() {
+    alert("You need to enter a search term!");
+}
+
+function editPost(id, post_text){
+    let edited_post = prompt("Edit you post", post_text);
+    
+}
