11c4fe1d0fa13f41c3cf1bf986021e34590df47c
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-04-28 12:48:32 +0100

    added image to profile page, fixed styling in new post

--- views/new_post-html.php
+++ views/new_post-html.php
@@ -1,11 +1,17 @@
 <?php
-$out = "<form enctype='multipart/form-data' action='index.php?page=home' method='POST' class='form-horizontal'>
-      <div class='form-group'>
-        <textarea class='form-control' rows='2' name='post' placeholder='Create a post...'></textarea>
-        <input type='file' name='image'>
-      </div>
-      <button type='submit' name='action' class='btn btn-default'>Post</button>
-    </form>";
+$out = "<div class='row'>
+          <div class='col-md-10'>
+              <form enctype='multipart/form-data' action='index.php?page=home' method='POST' class='form-horizontal'>
+                  <div class='form-group'>
+                    <textarea class='form-control' rows='2' name='post' placeholder='Create a post...'></textarea>
+                    <input type='file' name='image'>
+                  </div>
+              </form>
+          </div>
+          <div class='col-md-2'>
+                <button type='submit' name='action' class='btn btn-default btn-lg'>Submit</button>
+          </div>
+        </div>";
     
 echo $out;
 ?>
--- views/profile-html.php
+++ views/profile-html.php
@@ -5,8 +5,10 @@ $userDetailsFound = isset( $userDetails );
 if ( $userDetailsFound === false ) {
     echo 'no user found' ;
 }
-$userHTML = "<h5> Your profile: </h5>
-<ul id='userDetails'>";
+$userHTML = "<ol class='breadcrumb'>
+                <li class='active'>Your profile</li>
+            </ol>
+            <ul id='userDetails'>";
 while ( $user = $userDetails->fetchObject() ) {
 	$date_joined = substr($user->date_created, 0, 10); //cut off and show only date without time
 	$userHTML .= 
@@ -16,11 +18,19 @@ while ( $user = $userDetails->fetchObject() ) {
                 <input type='hidden' name='post_id' value='$user->location' />
         </div>
         <div class='panel-body'>
-            <h5><small>email:   </small>$user->email</h5>
-            <h5><small>joined:  </small>$date_joined</h5>
-            <h5><small>followers:   </small>$user->followers_count</h5>
-            <h5><small>posts:   </small>$user->posts_count</h5>
-            <h5><small>location:    </small>$user->location</h5>
+            <div class='row'>
+              <div class='col-md-4'>
+                <img src='uploads/profile.png' class='img-rounded' style='width:200px;height:200px;'>
+              </div>
+              <div class='col-md-8'>
+                  <h5><small>email:   </small>$user->email</h5>
+                  <h5><small>joined:  </small>$date_joined</h5>
+                  <h5><small>followers:   </small>$user->followers_count</h5>
+                  <h5><small>posts:   </small>$user->posts_count</h5>
+                  <h5><small>location:    </small>$user->location</h5>
+              </div>
+            </div>
+            
         </div>
         <div class='panel-footer'>
             <div id='googleMap' style='width:100%;height:200px;'>
@@ -31,7 +41,9 @@ while ( $user = $userDetails->fetchObject() ) {
      </li>"; 
 }
 $userHTML .= "</ul>
-<h5> Your posts: </h5>";
+<ol class='breadcrumb'>
+    <li class='active'>Your posts</li>
+</ol>";
 
 echo $userHTML;
 
